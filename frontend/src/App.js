5a1c99da52e9f03e2c8f8c80864b6089cca444a5
mMarzeta <94maciekm@gmail.com>
2017-08-03 21:15:58 +0200

    GET JSON from server and display it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            receivedData: "",
+        }
+    }
+
+    componentWillMount() {
+        axios({
+            method: 'get',
+            responseType: 'json',
+            url: 'http://127.0.0.1:8000/link-store/',
+
+        })
+            .then(response => {
+                this.setState({
+                    receivedData: response.data,
+                });
+                console.log("data: " + response + "status: " + response.status);
+            })
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                </div>
+                {JSON.stringify(this.state.receivedData)}
+            </div>
+        );
+    }
 }
 
 export default App;
